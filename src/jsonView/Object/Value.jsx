868cb4dc21b272e8d74b255d88e91fe586bc24fa
akshay upadhya <akshaysrinagar@gmail.com>
2019-01-18 14:19:03 +0530

    passing minimize function from key value and adding commas to at end of all vaalues

--- src/jsonView/Object/Value.jsx
+++ src/jsonView/Object/Value.jsx
@@ -6,15 +6,22 @@ import { DataType, Comma } from "../meta";
 import { Minimizer } from "../minimizer";
 export const minimizeContext = createContext();
 export const Value = props => {
-	let { depth, value, isArray, isLastElement } = props;
-	depth = depth || 0;
+	let { depth, value, isArray, isLastElement, isNodeMinimizable } = props;
+	depth = (depth || 0) + 1;
 	const json = JSON.parse(value);
 	const type = Utils.isObject(json)
 		? "Object"
 		: Utils.isArray(json)
 		? "Array"
 		: typeof (Number(value) || value === "true" || value === "false" || value);
-	const [isMinimized, minimize] = useState(depth >= 1);
+	let isMinimized, minimize;
+	if (isNodeMinimizable) {
+		// { isMinimized, minimize } = props;// i really dont know what is mistake here but it throws error
+		isMinimized = props.isMinimized;
+		minimize = props.minimize;
+	} else {
+		[isMinimized, minimize] = useState(depth >= 1);
+	}
 	return (
 		<minimizeContext.Provider value={{ isMinimized, minimize }}>
 			<div className="value">
@@ -24,16 +31,16 @@ export const Value = props => {
 					undefined
 				)}
 				{type === "Object" ? (
-					<JsonObject json={json} depth={depth + 1} />
+					<JsonObject json={json} depth={depth} />
 				) : type === "Array" ? (
-					<ArrayView json={json} depth={depth + 1} />
+					<ArrayView json={json} depth={depth} />
 				) : (
 					<Fragment>
 						<DataType DataType={type} />
 						<span className="generic">{value}</span>
 					</Fragment>
 				)}
-				{isArray && !isLastElement ? <Comma /> : undefined}
+				{!!!isLastElement ? <Comma /> : undefined}
 			</div>
 		</minimizeContext.Provider>
 	);
