015b715d46ae9cbe7e611c0643d8dedcda067091
akshay upadhya <akshaysrinagar@gmail.com>
2019-01-18 14:14:09 +0530

    adding minimiser for json key value pair

--- src/jsonView/Object/KeyValue.jsx
+++ src/jsonView/Object/KeyValue.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import { Comma, Tabs } from "../meta";
+import React, { useState } from "react";
+import { Utils } from "../utils/utils";
+import { Tabs } from "../meta";
 import { Key } from "../Object/Key";
 import { Value } from "../Object/Value";
+import { Minimizer } from "../minimizer";
 export const KeyValue = props => {
 	let { keyName, value, depth } = props;
 	depth = (depth ? depth : 0) + 1;
+	let isMinimized, minimize;
+	const isNodeMinimizable = Utils.isArray(value) || Utils.isObject(value);
+	if (isNodeMinimizable) {
+		[isMinimized, minimize] = useState(depth >= 1);
+	}
+	const newProps = { isNodeMinimizable, isMinimized, minimize };
 	return (
 		<div className="key-value">
-			<Tabs />
+			{isNodeMinimizable ? (
+				<Minimizer isMinimized={isMinimized} minimizer={minimize} />
+			) : (
+				<Tabs />
+			)}
 			<Key keyName={keyName} /> :
-			<Value value={JSON.stringify(value)} depth={depth} />
-			<Comma />
+			<Value value={JSON.stringify(value)} depth={depth} {...newProps} />
 		</div>
 	);
 };
